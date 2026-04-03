2bfd5a3a1f38f41cfc409d32fc2031300aa246a5
Obycodez55 <obikoyaadebayo55@gmail.com>
2026-04-03 20:56:41 +0100

    Update app configuration and structure: added scheme and experiments to app.json, removed App.tsx and index.ts, updated main entry in package.json, modified tsconfig.json to include additional TypeScript files.

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home is good for you</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
