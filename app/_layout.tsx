6116a0e0c74aa5a7bd4d6424a6355a72d2790796
Obycodez55 <obikoyaadebayo55@gmail.com>
2026-04-03 23:12:48 +0100

    Enhance Home component layout: added logo, updated text, and included navigation links for About and Contact pages. Improved styling for better presentation.

---
+++ app/_layout.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Stack } from "expo-router";
+
+const RootLayout = () => {
+    const colorScheme = useColorScheme();
+    console.log(colorScheme);
+    
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#ddd" },
+        headerTintColor: "#333",
+       }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+      <Stack.Screen name="contact" options={{ title: "Contact" }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
